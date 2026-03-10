5943a26f2598137e528d108b5de73283bc4e44ae
thabithapanneerselvam-RQ <thabitha.panneerselvam@rootquotient.com>
2026-03-10 15:20:57 +0530

    query parser added

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,7 @@ const app = express()
 
 app.use(express.json())
 app.use(bodyParser.json())
+app.set("query parser", "extended");
 
 app.get("/api/user", (_req:Request, res:Response)=>{
     res.json({
